b1360669a7330f1dd0c304ef94b308193edf05a8
Aaron Patterson <aaron.patterson@gmail.com>
2012-10-04 10:39:49 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+Lolwut::Application.config.secret_token = 'f9bea49a1c726a0493c6701e80c2f7ecd8a20ca150ec3584b431ce9b59aabeb0034ef3eeacf46cdd688412745e0bcceec4bab75d94f89ab3831093d14f14f1d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lolwut::Application.config.session_store :cookie_store, key: '_lolwut_session'
